a5c9dbf61df552e9c6b74866e6849ebcfd0d9334
Olekrr <93515165+Olekrr@users.noreply.github.com>
2023-12-17 22:59:17 +0100

    Added toggeling to login/logout button

--- src/js/api/header/headersetup.js
+++ src/js/api/header/headersetup.js
@@ -1,20 +1,34 @@
 import { getUserData } from "./userdata.js";
+import { loadModal } from "../../loginmodal/loadform.js";
 
 export async function setupHeader() {
   await refreshHeader();
-
-  const logoutButton = document.getElementById("logoutButton");
-  logoutButton.addEventListener("click", () => {
-    localStorage.clear();
-    window.location.reload();
-  });
 }
 
 export async function refreshHeader() {
   const userData = await getUserData();
+  const authButton = document.getElementById("logoutButton");
+
   if (userData) {
     document.getElementById("userProfilePic").src = userData.avatar;
     document.getElementById("userCredits").textContent =
       `Credits: ${userData.credits}`;
+    authButton.textContent = "Logout";
+    authButton.classList.remove("btn-primary");
+    authButton.classList.add("btn-outline-danger");
+    authButton.removeEventListener("click", loadModal);
+    authButton.addEventListener("click", logoutFunction);
+  } else {
+    document.getElementById("userCredits").textContent = "";
+    authButton.textContent = "Login";
+    authButton.classList.remove("btn-outline-danger");
+    authButton.classList.add("btn-primary");
+    authButton.removeEventListener("click", logoutFunction);
+    authButton.addEventListener("click", loadModal);
   }
+}
+
+function logoutFunction() {
+  localStorage.clear();
+  window.location.reload();
 }
