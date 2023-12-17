dd61f7911deace7cdd936fd9d04cd10a79f6e941
Olekrr <93515165+Olekrr@users.noreply.github.com>
2023-12-17 20:28:55 +0100

    refactored login.js and register.js to make use of apiservice.js

--- src/js/api/register.js
+++ src/js/api/register.js
@@ -1,17 +1,13 @@
-import { baseUrl } from "./urlbase.js";
+import { fetchData } from "./apiservice.js";
 
 export async function register(username, email, password, avatarUrl) {
-  const response = await fetch(`${baseUrl}/auction/auth/register`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      name: username,
-      email,
-      password,
-      avatar: avatarUrl,
-    }),
-  });
-  return response.ok
-    ? await response.json()
-    : Promise.reject(await response.json());
+  const userData = {
+    name: username,
+    email,
+    password,
+    avatar: avatarUrl,
+  };
+
+  const response = await fetchData("auth/register", "POST", userData);
+  return response;
 }
