8460863628c6b8997343998441d900535f7d96a3
Olekrr <93515165+Olekrr@users.noreply.github.com>
2023-12-17 23:09:33 +0100

    added styling to bid history

--- src/js/api/tabscontent/bidding/bidhistory.js
+++ src/js/api/tabscontent/bidding/bidhistory.js
@@ -1,14 +1,18 @@
 export function bidHistory(bids) {
   const sortedBids = bids.sort((a, b) => b.amount - a.amount);
-  let bidHistoryHtml = '<ul class="list-unstyled">';
+  let bidHistoryHtml = '<ul class="list-group list-group-flush">'; // Using Bootstrap's list group for styling
   if (sortedBids.length > 0) {
     sortedBids.forEach((bid) => {
-      bidHistoryHtml += `<li>${bid.bidderName}: $${bid.amount} (on ${new Date(
-        bid.created,
-      ).toLocaleString()})</li>`;
+      bidHistoryHtml += `
+        <li class="list-group-item">
+          <strong>${bid.bidderName}</strong>: $${bid.amount}
+          <span class="text-muted" style="float: right;">(on ${new Date(
+            bid.created,
+          ).toLocaleString()})</span>
+        </li>`;
     });
   } else {
-    bidHistoryHtml += "<li>No bids yet.</li>";
+    bidHistoryHtml += '<li class="list-group-item">No bids yet.</li>';
   }
   bidHistoryHtml += "</ul>";
   return bidHistoryHtml;
